89333a98438ae79faad9ed43015f3dc1cefb97c7
Gemma Pedrini <gemma.pedrini@gmail.com>
2019-01-16 01:29:35 +0100

    aggiunto file di output

--- generate.cmd
+++ generate.cmd
@@ -1,7 +1,17 @@
-pandoc ".\Parte 1.md" --reference-doc=reference.docx -s -o ".\generated\Parte 1.docx" 
-pandoc -s ".\Parte 2.md" --reference-doc=reference.docx  -o ".\generated\Parte 2.docx" 
-pandoc -s ".\Parte 3.md" --reference-doc=reference.docx  -V fontsize=13pt -o ".\generated\Parte 3.docx" 
-pandoc -s ".\todo.md" --reference-doc=reference.docx -o ".\generated\todo.docx" 
-pandoc -s ".\README.md" --reference-doc=reference.docx  -o ".\generated\README.docx" 
-pandoc -s ".\Ale.md" --reference-doc=reference.docx -o ".\generated\Ale.docx" 
-pandoc -s ".\help.md" --reference-doc=reference.docx -o ".\generated\help.docx"
+@echo off
+del output.txt
+echo "Generating Parte 1"  >> output.txt 2>&1
+pandoc ".\Parte 1.md" --reference-doc=reference.docx -s -o ".\generated\Parte 1.docx" >> output.txt 2>&1
+echo "Generating Parte 2"  >> output.txt 2>&1
+pandoc -s ".\Parte 2.md" --reference-doc=reference.docx  -o ".\generated\Parte 2.docx" >> output.txt 2>&1
+echo "Generating Parte 3"  >> output.txt 2>&1
+pandoc -s ".\Parte 3.md" --reference-doc=reference.docx  -V fontsize=13pt -o ".\generated\Parte 3.docx"  >> output.txt 2>&1
+echo "Generating todo"  >> output.txt 2>&1
+pandoc -s ".\todo.md" --reference-doc=reference.docx -o ".\generated\todo.docx" >> output.txt 2>&1
+echo "Generating README"  >> output.txt 2>&1
+pandoc -s ".\README.md" --reference-doc=reference.docx  -o ".\generated\README.docx" >> output.txt 2>&1
+echo "Generating Ale"  >> output.txt 2>&1
+pandoc -s ".\Ale.md" --reference-doc=reference.docx -o ".\generated\Ale.docx" >> output.txt 2>&1
+echo "Generating help"  >> output.txt 2>&1
+pandoc -s ".\help.md" --reference-doc=reference.docx -o ".\generated\help.docx" >> output.txt 2>&1
+
--- push.cmd
+++ push.cmd
@@ -1,4 +1,10 @@
-SET COMMIT=modifica di gemma1
-git add .
-git commit -a -m "%COMMIT%"
-git push
+@ECHO OFF
+del output.txt
+SET /p COMMIT=<commit.txt
+echo commit = %COMMIT% >> output.txt 2>&1
+echo Loading current changes >> output.txt 2>&1
+git add . >> output.txt 2>&1
+echo Doing the commit >> output.txt 2>&1
+git commit -a -m "%COMMIT%" >> output.txt 2>&1
+echo Making push... >> output.txt 2>&1
+git push >> output.txt 2>&1
